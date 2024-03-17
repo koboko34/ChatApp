4480e3c4b2dac1b8c2449f5a78f56abf78cc03b2
Armand Yilinkou <66181314+koboko34@users.noreply.github.com>
2024-03-17 18:11:42 +0000

    Fixed NullPointerException in Printer

--- src/network/Printer.java
+++ src/network/Printer.java
@@ -60,8 +60,8 @@ public class Printer implements Runnable {
 			}
 		}
 		
-		if (pingerThread.isAlive()) {
-			pingerThread.interrupt();			
+		if (pingerThread != null && pingerThread.isAlive()) {
+			pingerThread.interrupt();
 		}
 	}
 }
